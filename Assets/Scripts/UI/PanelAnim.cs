e97aba6bf49bd70234fe269f171df9602a68ac3a
LHJ_LAPTOP\25833 <2583371339@qq.com>
2026-03-29 16:41:44 +0800

    加入背景ui，有点小bug

---
+++ Assets/Scripts/UI/PanelAnim.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelAnim : MonoBehaviour
+{
+    public AnimationCurve showCurve;
+    public AnimationCurve hideCurve;
+    public float animationSpeed;
+
+    private bool isShowed =false;
+    IEnumerator ShowPanel()
+    {
+        float timer = 0;
+        while (timer <= 1)
+        {
+            this.transform.localScale = Vector3.one*showCurve.Evaluate(timer);
+            timer += Time.deltaTime * animationSpeed;
+            yield return null;
+        }
+    }
+
+    IEnumerator HidePanel()
+    {
+        float timer = 0;
+        while (timer <= 1)
+        {
+            this.transform.localScale = Vector3.one * hideCurve.Evaluate(timer);
+            timer += Time.deltaTime * animationSpeed;
+            yield return null;
+        }
+    }
+
+    public void  Show()
+    {
+        Debug.Log("ÏÔÊ¾");
+        if (!isShowed)
+        {
+            StartCoroutine(ShowPanel());
+            isShowed = true;
+        }
+    }
+    public void Hide()
+    {
+        if (isShowed)
+        {
+            StartCoroutine(HidePanel());
+            isShowed = false;
+        }
+    }
+    public void GameOver()
+    {
+        Application.Quit();
+    }
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Debug.Log("ÏÔÊ¾");
+    //        Show();
+            
+    //    }
+    //    if (Input.GetMouseButtonDown(1))
+    //    {
+    //        Hide();
+    //    }
+    //}
+}
